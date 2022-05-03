2fb5e1f3fe0f51991a09f9f5fa77b0a52a885de1
qprquo <qprquo@gmail.com>
2022-05-03 22:54:18 +0300

    feat: add mobile menu

--- source/js/app.js
+++ source/js/app.js
@@ -5,3 +5,16 @@ function changeSliderValue() {
   slider.style.setProperty('--slider-value', sliderRange.value + '%');
 }
 
+const menu = document.querySelector('.js-menu');
+document.addEventListener('click', function (_e) {
+  menu.classList.remove('nav__list--opened');
+});
+
+const toggle = document.querySelector('.js-menu-toggle');
+if (toggle) {
+  toggle.addEventListener('click', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    menu.classList.toggle('nav__list--opened');
+  });
+}
